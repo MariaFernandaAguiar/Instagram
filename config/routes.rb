1c04a4f1521a33537c6185804dd3d42554a402fa
maria <abreuaguiar07@gmail.com>
2023-03-21 14:56:48 -0400

    feat: add comment functionality to posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   # get 'home/index'
-  root to: 'home#index'
+  root to: "home#index"
   
   post "toggle_like", to:"likes#toggle_like", as: :toggle_like
+
+  resources :comments, only:[:create, :destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
